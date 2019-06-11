33524bf70d39b51d78de3c6a36772726c93f9996
Rob <Rob@Onev>
2019-06-12 01:06:33 +0800

    验证：点击控件取消输入框键盘

---
+++ miniprogram/pages/clickViewCancelKeyboard/clickViewCancelKeyboard.js
@@ -0,0 +1,12 @@
+// pages/clickViewCancelKeyboard/clickViewCancelKeyboard.js
+Page({
+
+  'tapView'() {
+    wx.showToast({
+      title: '消息响应',
+      icon: 'success',
+      duration: 1000
+    })
+  }
+
+});
