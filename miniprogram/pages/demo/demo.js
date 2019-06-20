1a230da9d971343d5be686150001249016ad3a98
Rob <Rob@Onev>
2019-06-20 22:40:05 +0800

    U⏐更新：input-group输入控件组的demo

--- miniprogram/pages/demo/demo.js
+++ miniprogram/pages/demo/demo.js
@@ -3,16 +3,24 @@ Page({
 
   data: {
     value: 'String',
-    style: 'Q123',
-    name: '夏装连衣裙',
+    style: 'Q123',      style1: '98991',
+    name: '夏装连衣裙',  name1: '冬装夏穿',
+    group: {
+      style1: false,
+    },
   },
 
-  onInput,
+  onInput, onTapMore,
   showInputValue,
 });
 
 function onInput(e) {
-  this.setData({ [e.detail.componentName]: e.detail.value });
+  // this.setData({ [e.detail.name]: e.detail.value })
+  this.data[e.detail.name] = e.detail.value
+}
+
+function onTapMore(e) {
+  this.data.group[e.detail.name] = e.detail.more
 }
 
 function showInputValue(e) {
