5665942dc888b74901366efe7d6084dbe4fc6ca2
Rob <Rob@Onev>
2019-06-22 20:25:44 +0800

    U⏐更新：input-combo组件限制价格输入的小数点位数
    限制为2位小数点

--- uikit/src/input-combo/index.js
+++ uikit/src/input-combo/index.js
@@ -12,7 +12,9 @@ Component({
   },
 
   // Component initial data
-  data: {},
+  data: {
+    maxlength: 140, // 输入框最大长度，系统默认值
+  },
 
   // Component methods
   methods: {
@@ -22,7 +24,17 @@ Component({
 
 // 键盘输入的事件响应，外部通过 name 区分组件实例
 function onInput(e) {
-  this.setData({ value: e.detail.value });
+  // 是价格输入框吗？
+  if ( this.properties.name === 'price' ) {
+    const dot = e.detail.value.indexOf('.');
+    if ( dot >= 0 ) { // 有小数点，最多只能继续输入2个字符
+      this.data.maxlength = dot + 3
+    } else {          // 没有小数点，最大长度放开为系统限制
+      this.data.maxlength = 140
+    }
+  }
+
+  this.setData({ value: e.detail.value, maxlength: this.data.maxlength });
   e.detail.name = this.properties.name;
   this.triggerEvent('input', e.detail)
 }
