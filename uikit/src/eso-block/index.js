95a75abdc91c18fe65ef59f3d5600f0449a3011b
Rob <Rob@Onev>
2019-06-25 04:03:44 +0800

    A⏐新增：UI套件￤eso-block 标题内容块

---
+++ uikit/src/eso-block/index.js
@@ -0,0 +1,18 @@
+// eso-block/index.js
+
+Component({
+  externalClasses: ['com-class','title-class','link-class', 'block-class'],
+
+  // Component properties
+  properties: {
+    title:  String,   // 区块标题，左上侧
+    link:   String,   // 区块链接，右上侧
+    url:    String,   // 区块链接目标
+  },
+
+  // Component initial data
+  data: {},
+
+  // Component methods
+  methods: {},
+});
