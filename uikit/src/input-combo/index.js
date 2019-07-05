9fadd0fcfba409748b7f41441ed847a616b6ca61
Rob <Rob@Onev>
2019-07-05 20:47:08 +0800

    F⏐修复：UI套件包￤input-combo & input-group 增加原生input控件若干属性
    maxlength设置默认值，属性名改为驼峰形式

--- uikit/src/input-combo/index.js
+++ uikit/src/input-combo/index.js
@@ -11,13 +11,16 @@ Component({
     type:               String,
     value:              String,
     disabled:           Boolean,
-    maxlength:          Number,   // 输入框最大长度，系统默认值
+    maxlength: {                  // 输入框最大长度
+      type:             Number,
+      value:            140,      // 系统默认值
+    },
     focus:              Boolean,
-    'confirm-type':     String,
-    'confirm-hold':     Boolean,
+    confirmType:        String,
+    confirmHold:        Boolean,
     cursor:             Number,
-    'selection-start':  Number,
-    'selection-end':    Number,
+    selectionStart:     Number,
+    selectionEnd:       Number,
   },
 
   // Component initial data
@@ -47,7 +50,7 @@ function onInput(e) {
     const dot = e.detail.value.indexOf('.');
     if ( dot >= 0 ) { // 有小数点，最多只能继续输入2个字符
       this.properties.maxlength = dot + 3
-    } else {          // 没有小数点，最大长度放开为系统限制
+    } else {          // 没有小数点，最大长度放开为初始值
       this.properties.maxlength = this.data._maxlength
     }
   }
--- uikit/src/input-group/index.js
+++ uikit/src/input-group/index.js
@@ -11,18 +11,26 @@ Component({
     type:               String,
     value:              String,
     disabled:           Boolean,
-    maxlength:          Number,   // 输入框最大长度，系统默认值
+    maxlength: {                  // 输入框最大长度
+      type:             Number,
+      value:            140,      // 系统默认值
+    },
     focus:              Boolean,
-    'confirm-type':     String,
-    'confirm-hold':     Boolean,
+    confirmType:        String,
+    confirmHold:        Boolean,
     cursor:             Number,
-    'selection-start':  Number,
-    'selection-end':    Number,
+    selectionStart:     Number,
+    selectionEnd:       Number,
     more:               Boolean,
   },
 
   // Component initial data
-  data: {},
+  data: {
+    _maxlength:         140,      // 输入框最大长度，外部传入长度限制
+  },
+
+  // Component lifecycle function
+  lifetimes: { ready },
 
   // Component methods
   methods: {
@@ -31,9 +39,24 @@ Component({
   },
 });
 
+// 组件Ready
+function ready() {
+  this.setData({ _maxlength: this.properties.maxlength })
+}
+
 // 键盘输入的事件响应，外部通过 name 区分组件实例
 function onInput(e) {
-  this.setData({ value: e.detail.value });
+  // 是价格输入框吗？
+  if ( this.properties.name === 'price' ) {
+    const dot = e.detail.value.indexOf('.');
+    if ( dot >= 0 ) { // 有小数点，最多只能继续输入2个字符
+      this.properties.maxlength = dot + 3
+    } else {          // 没有小数点，最大长度放开为初始值
+      this.properties.maxlength = this.data._maxlength
+    }
+  }
+
+  this.setData({ value: e.detail.value, maxlength: this.properties.maxlength });
   e.detail.name = this.properties.name;
   this.triggerEvent('input', e.detail)
 }
